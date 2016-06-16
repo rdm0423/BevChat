405d0be831322d792f3a631b8d22ff37c5380e0f
Ross McIlwaine <rdm0423@gmail.com>
2016-06-16 12:10:28 -0600

    Adds Check Logic for TextField Entry

--- BevChat/CreateAccountViewController.swift
+++ BevChat/CreateAccountViewController.swift
@@ -18,7 +18,6 @@ class CreateAccountViewController: UIViewController, UIImagePickerControllerDele
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -60,43 +59,138 @@ class CreateAccountViewController: UIViewController, UIImagePickerControllerDele
     
     @IBAction func createAccountButtonTapped(sender: AnyObject) {
         
-        if let displayNameTextField = displayNameTextField.text, firstNameTextField = firstNameTextField.text, lastNameTextField = lastNameTextField.text, email = emailTextField.text, password = passwordTextField.text {
+        textFieldVerifyForTextEntry()
+        
+        if let displayNameTextField = displayNameTextField.text, firstNameTextField = firstNameTextField.text, lastNameTextField = lastNameTextField.text, email = emailTextField.text, password = passwordTextField.text, profileImageView = UIImage.init(base64: "") {
             
             
-            // Also need to create the user with the other saved information ****
+            // *** Also need to create the user with the other saved information -- just call create user (create user authenticates) ****
             
-            // be sure cannot create unless matching password **
+            // *** be sure cannot create unless matching password & all text fields are entered**
             
-            UserController.authUser(email, password: password, completion: { (user) in
-                print(user)
+            UserController.createUser(firstNameTextField, lastName: lastNameTextField, displayName: displayNameTextField, profilePhoto: profileImageView, email: email, password: password, completion: { (user) in
+                
                 self.performSegueWithIdentifier("toHomeSegue", sender: self)
             })
         }
-    
     }
 
     @IBAction func alreadyHaveAccountButtonTapped(sender: AnyObject) {
+        // segue in storyboard exists to take to LogIn VC
     }
+    
+    // MARK: - Image Selection Controller
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         
         guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
             return
         }
-        imageView.image = image
+        profileImageView.image = image
         dismissViewControllerAnimated(true, completion: nil)
     }
     
-    func passwordVerify() {
+    // MARK: - Textfield entry Logic Tests
+    
+    func textFieldVerifyForTextEntry() {
         
-        if passwordTextField.text == confirmPasswordTextField.text {
-            // successful enter of matching password
-        } else {
-            // error - mistyped password characters
-            
-            // present alert
+        if displayNameTextField.text == nil || firstNameTextField.text == nil || lastNameTextField.text == nil || emailTextField.text == nil || passwordTextField.text == nil || confirmPasswordTextField.text == nil {
+            // did not enter text into all fields
+            emptyTextFieldErrorAlert()
+        } else if isValidEmail("\(emailTextField.text)") == false {
+            // not a valid email entered
+            emailErrorAlert()
+        } else if containsOnlyLettersAndNumbers("\(displayNameTextField.text)") == false {
+            // not a valid display name
+            displayNameContainsSpecialCharactersErrorAlert()
+        } else if isValidDisplayNameAvailible("\(displayNameTextField.text)") == false {
+            // display name is taken
+            displayNameTakenErrorAlert()
+        } else if passwordVerify(passwordTextField.text!, testString2: confirmPasswordTextField.text!) == false { // FORCE UNWRAP - the textfield exists
             passwordErrorAlert()
         }
+    }
+    
+    func isValidDisplayNameAvailible(testString: String) -> Bool {
+        
+        // check it is availible
+        // check it is xMin characters
+        
+        return true
+    }
+    
+    func containsOnlyLettersAndNumbers(input: String) -> Bool {
+        for chr in input.characters {
+            if (!(chr >= "a" && chr <= "z") && !(chr >= "A" && chr <= "Z") && !(chr >= "0" && chr <= "9")) {
+                return false
+            }
+        }
+        // Valid with only letters and numbers
+        return true
+    }
+    
+    func isValidEmail(testString:String) -> Bool {
+        // print("validate calendar: \(testStr)")
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluateWithObject(testString)
+    }
+    
+    func passwordVerify(testString1: String, testString2: String) -> Bool {
+        
+        if testString1 == testString2 {
+            // successful enter of matching password
+            return true
+        } else {
+            // error - mistyped password characters
+            return false
+        }
+    }
+    
+    // MARK: - Alert Controllers
+    
+    func emptyTextFieldErrorAlert() {
+        
+        let alertController = UIAlertController(title: "ERROR", message: "Please be sure you complete all TextFields. /n Please try again", preferredStyle: .Alert)
+        
+        let dismissAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        
+        alertController.addAction(dismissAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func displayNameTakenErrorAlert() {
+        
+        let alertController = UIAlertController(title: "DISPLAY NAME ERROR", message: "This display name is already taken, please try another name. /n Please try again", preferredStyle: .Alert)
+        
+        let dismissAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        
+        alertController.addAction(dismissAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func displayNameContainsSpecialCharactersErrorAlert() {
+        
+        let alertController = UIAlertController(title: "DISPLAY NAME ERROR", message: "The display name cannot contain special characters. /n Please try again with letters and numbers", preferredStyle: .Alert)
+        
+        let dismissAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        
+        alertController.addAction(dismissAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func emailErrorAlert() {
+        
+        let alertController = UIAlertController(title: "EMAIL ERROR", message: "Please be sure you enter a valid email address. /n Please try again", preferredStyle: .Alert)
+        
+        let dismissAction = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        
+        alertController.addAction(dismissAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
     
     func passwordErrorAlert() {
